e4b3d6594fdd2c4e8e4959d32390ce14021bb70e
kakzumooto <kakzumooto22@gmail.com>
2025-04-25 17:58:24 -0600

    Prueba de valores

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const niveles = [
     `public class HolaMundo {
       public static void main(String[] args) {
-          System.out.println("¡Hola, mundo!");
+          System.out.println("¡Hello, mundo!");
       }
     }`,
 
